d78b83b7a1adaba2c2a4bcb192184710d5a3ec8c
Jan Kvetina <jan.kvetina@gmail.com>
2023-09-19 22:05:46 +0200

    Grants

---
+++ database/grants/APPS_schema.sql
@@ -0,0 +1,13 @@
+GRANT CONNECT               TO apps;
+--
+GRANT ALTER SESSION                     TO apps;
+GRANT CREATE JOB                        TO apps;
+GRANT CREATE MATERIALIZED VIEW          TO apps;
+GRANT CREATE PROCEDURE                  TO apps;
+GRANT CREATE SEQUENCE                   TO apps;
+GRANT CREATE SYNONYM                    TO apps;
+GRANT CREATE TABLE                      TO apps;
+GRANT CREATE TRIGGER                    TO apps;
+GRANT CREATE VIEW                       TO apps;
+GRANT UNLIMITED TABLESPACE              TO apps;
+
